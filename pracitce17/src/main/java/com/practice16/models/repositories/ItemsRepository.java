d4533e838a98171a426f753285a5284d53061ab9
Danila <kimoror@mail.ru>
2021-04-07 01:29:53 +0300

    practice 17, 18, 19 are ready

--- pracitce17/src/main/java/com/practice16/models/repositories/ItemsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice16.models.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.practice16.models.entities.Items;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemsRepository extends JpaRepository<Items, Integer> { }
--- pracitce17/src/main/java/com/practice16/models/repositories/OrdersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice16.models.repositories;
-
-import com.practice16.models.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrdersRepository extends JpaRepository<Order, Integer>{}
